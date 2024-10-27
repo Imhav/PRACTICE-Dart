0366efd4bab929e9a1b39c1dc68edb1b3e9ae7d8
Imhav <ouvenhav500@gmail.com>
2024-10-27 22:29:51 +0700

    Update ex1.dart

--- W3-S2/ex1.dart
+++ W3-S2/ex1.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Week1());
+  runApp(const Ex1());
 }
 
-class Week1 extends StatelessWidget {
-  const Week1({super.key});
+class Ex1 extends StatelessWidget {
+  const Ex1({super.key});
 
   @override
   Widget build(BuildContext context) {
