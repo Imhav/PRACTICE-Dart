cd01dcf3b1ddda732846a7adc55fef43300f1ed6
Imhav <ouvenhav500@gmail.com>
2024-11-24 16:51:03 +0700

    quiz_pratice

---
+++ week7/section1_pratice_quiz/data/question.dart
@@ -0,0 +1,133 @@
+import '../model/quiz/quiz.dart';
+
+
+List<Quiz> quizzes = [
+  Quiz(
+    title: "",
+    questions: [
+      const Question(
+        title: "Who is the best teacher?",
+        possibleAnswers: ["ronan", "hongly", 'leangsiv'],
+        goodAnswer: "ronan",
+      ),
+      const Question(
+        title: "What Subject is he teach ",
+        possibleAnswers: ["Java", "C++", "Flutter"],
+        goodAnswer: "Flutter",
+      ),
+      const Question(
+        title: "What is his favorite food?",
+        possibleAnswers: ["fruit", "bread", "rice"],
+        goodAnswer: "rice",
+      ),
+      const Question(
+        title: "What planet is known as the Red Planet?",
+        possibleAnswers: ["Earth", "Mars", "Jupiter"],
+        goodAnswer: "Mars",
+      ),
+      const Question(
+        title: "Who was the first president of the United States?",
+        possibleAnswers: ["George Washington", "Thomas Jefferson", "Abraham Lincoln"],
+        goodAnswer: "George Washington",
+      ),
+      /*const Question(
+        title: "In which year did the Titanic sink?",
+        possibleAnswers: ["1912", "1915", "1920"],
+        goodAnswer: "1912",
+      ),
+      const Question(
+        title: "What is the capital of France?",
+        possibleAnswers: ["Berlin", "Madrid", "Paris"],
+        goodAnswer: "Paris",
+      ),
+      const Question(
+        title: "Which continent is the Sahara Desert located on?",
+        possibleAnswers: ["Asia", "Africa", "Australia"],
+        goodAnswer: "Africa",
+      ),
+      const Question(
+        title: "What is the value of Pi to two decimal places?",
+        possibleAnswers: ["3.14", "2.17", "1.62"],
+        goodAnswer: "3.14",
+      ),
+      const Question(
+        title: "What is 5 multiplied by 6?",
+        possibleAnswers: ["30", "25", "35"],
+        goodAnswer: "30",
+      ),
+      const Question(
+        title: "Who wrote 'Romeo and Juliet'?",
+        possibleAnswers: ["William Shakespeare", "Charles Dickens", "Jane Austen"],
+        goodAnswer: "William Shakespeare",
+      ),
+      const Question(
+        title: "Which novel begins with 'Call me Ishmael'?",
+        possibleAnswers: ["Moby Dick", "War and Peace", "The Great Gatsby"],
+        goodAnswer: "Moby Dick",
+      ),
+      */
+    ],
+  ),
+/*  Quiz(
+    title: "History Quiz",
+    questions: [
+      const Question(
+        title: "Who was the first president of the United States?",
+        possibleAnswers: ["George Washington", "Thomas Jefferson", "Abraham Lincoln"],
+        goodAnswer: "George Washington",
+      ),
+      const Question(
+        title: "In which year did the Titanic sink?",
+        possibleAnswers: ["1912", "1915", "1920"],
+        goodAnswer: "1912",
+      ),
+    ],
+  ),
+
+  Quiz(
+    title: "Geography Quiz",
+    questions: [
+      const Question(
+        title: "What is the capital of France?",
+        possibleAnswers: ["Berlin", "Madrid", "Paris"],
+        goodAnswer: "Paris",
+      ),
+      const Question(
+        title: "Which continent is the Sahara Desert located on?",
+        possibleAnswers: ["Asia", "Africa", "Australia"],
+        goodAnswer: "Africa",
+      ),
+    ],
+  ),
+  Quiz(
+    title: "Math Quiz",
+    questions: [
+      const Question(
+        title: "What is the value of Pi to two decimal places?",
+        possibleAnswers: ["3.14", "2.17", "1.62"],
+        goodAnswer: "3.14",
+      ),
+      const Question(
+        title: "What is 5 multiplied by 6?",
+        possibleAnswers: ["30", "25", "35"],
+        goodAnswer: "30",
+      ),
+    ],
+  ),
+  Quiz(
+    title: "Literature Quiz",
+    questions: [
+      const Question(
+        title: "Who wrote 'Romeo and Juliet'?",
+        possibleAnswers: ["William Shakespeare", "Charles Dickens", "Jane Austen"],
+        goodAnswer: "William Shakespeare",
+      ),
+      const Question(
+        title: "Which novel begins with 'Call me Ishmael'?",
+        possibleAnswers: ["Moby Dick", "War and Peace", "The Great Gatsby"],
+        goodAnswer: "Moby Dick",
+      ),
+    ],
+  ),
+  */
+];
---
+++ week7/section1_pratice_quiz/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'widgets/quiz_app.dart';
+import '../section1_pratice_quiz/data/question.dart';
+
+void main() {
+  runApp(QuizApp(quiz: quizzes[0])); 
+}
